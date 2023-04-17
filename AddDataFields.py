debd7541d37a8b0fbee9cdc190f7bc8c32eec39c
nicholasrosenorn <nrosenor@purdue.edu>
2023-04-16 21:19:20 -0400

    sector/industry labels

--- AddDataFields.py
+++ AddDataFields.py
@@ -20,6 +20,7 @@
 import pandas as pd
 import numpy as np
 import math
+import yfinance as yf
 
 
 #column indexes
@@ -219,9 +220,6 @@ def interest_rates(df):
 
     return df
 
-#TODO
-#label if compnay is a pharmacutical company
-#def pharmIndustry():
 
 #TODO
 #attaches return of 1 year T-bill to each date
